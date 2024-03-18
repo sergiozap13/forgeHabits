aebf418621ad23459d4aad7910ffa6348751caf1
sergio zapata <sergiozap1@gmail.com>
2024-03-18 16:31:07 +0100

    controlador de las instrucciones recogiendo de la base de datos

--- backend/src/controllers/instructionsController.js
+++ backend/src/controllers/instructionsController.js
@@ -2,13 +2,13 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function getInstructionsById (req, res) {
-  const instructions = await prisma.instructions.findMany({
+  const instructions = await prisma.instruction.findMany({
     where: {
       category: req.params.category
     }
   })
 
-  if (instructions === null) { res.status(404).json({ message: 'Instructions not found' }) }
+  if (instructions.length === 0) { res.status(404).json({ message: 'Instructions not found' }) }
 
   res.json(instructions)
 }
