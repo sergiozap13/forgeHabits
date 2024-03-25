b959d6fb7cdbc6b40db8f097f76036c973722643
sergio zapata <sergiozap1@gmail.com>
2024-03-25 13:37:32 +0100

    app.js n index.js changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,11 @@
 // archivo con el servidor
 import express, { json } from 'express'
 import { corsMiddleware } from './middlewares/cors.js'
+import { habitsRouter } from './routes/habits.js'
+import { calendarRouter } from './routes/calendar.js'
+import { diaryRouter } from './routes/diary.js'
+import { instructionsRouter } from './routes/instructions.js'
+import { usersRouter } from './routes/users.js'
 
 // variables globales
 const app = express()
@@ -8,5 +13,16 @@ const app = express()
 app.use(json())
 app.use(corsMiddleware())
 app.disable('x-powered-by')
+
+// rutas
+app.use('/api/habits', habitsRouter)
+app.use('/api/calendar', calendarRouter)
+app.use('/api/diary', diaryRouter)
+app.use('/api/instructions', instructionsRouter)
+app.use('/api/users', usersRouter)
+// para aquellas páginas que no existan.
+app.use((req, res) => {
+  res.status(404).send('<h1>404 error</h1>')
+})
 
 export default app
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,23 +3,8 @@ import app from './app.js'
 import dotenv from 'dotenv'
 
 import { PORT } from './config.js'
-import { habitsRouter } from './routes/habits.js'
-import { calendarRouter } from './routes/calendar.js'
-import { diaryRouter } from './routes/diary.js'
-import { instructionsRouter } from './routes/instructions.js'
-import { usersRouter } from './routes/users.js'
 
 dotenv.config()
-// rutas
-app.use('/api/habits', habitsRouter)
-app.use('/api/calendar', calendarRouter)
-app.use('/api/diary', diaryRouter)
-app.use('/api/instructions', instructionsRouter)
-app.use('/api/users', usersRouter)
-// para aquellas páginas que no existan.
-app.use((req, res) => {
-  res.status(404).send('<h1>404 error</h1>')
-})
 
 app.listen(PORT, () => {
   console.log(`server listening on http://localhost:${PORT}`)
