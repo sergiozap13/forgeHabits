1cf862ad40f2e3bdf10654ce74e7ea7a54b0ae46
sergio zapata <sergiozap1@gmail.com>
2024-04-09 16:14:24 +0200

    controlador de rutas de authorization con JWT

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,4 +1,5 @@
 import passport from 'passport'
+import jwt from 'jsonwebtoken'
 
 async function login (req, res, next) {
   passport.authenticate('local', (err, user, info) => {
@@ -8,46 +9,74 @@ async function login (req, res, next) {
     if (!user) {
       return res.status(401).json({ success: false, message: info.message })
     }
-    req.logIn(user, (loginErr) => {
-      if (loginErr) {
-        return next(loginErr)
-      }
-      return res.json({ success: true, message: 'Login exitoso', user: { id: user.id, email: user.email } })
-    })
+    const payload = { id: user.id, email: user.email }
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' })
+    return res.json({ success: true, message: 'Login exitoso', token })
   })(req, res, next)
 }
 
-async function logout (req, res) {
-  if (req.sessionID) {
-    req.session.destroy((err) => {
-      if (err) {
-        return res.status(500).json({ error: 'Error al cerrar la sesión' })
-      }
-      res.clearCookie('connect.sid')
-      res.json({ success: true, message: 'Sesión cerrada correctamente' })
-    })
+// TODO: eliminar servicio
+// async function logout (req, res) {
+//   if (req.sessionID) {
+//     req.session.destroy((err) => {
+//       if (err) {
+//         return res.status(500).json({ error: 'Error al cerrar la sesión' })
+//       }
+//       res.clearCookie('connect.sid')
+//       res.json({ success: true, message: 'Sesión cerrada correctamente' })
+//     })
+//   } else {
+//     res.json({ success: true, message: 'No hay sesión abierta' })
+//   }
+// }
+
+async function status (req, res) {
+  const token = req.headers.authorization?.split(' ')[1]
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      res.json({ isLoggedIn: true, user: decoded })
+    } catch (err) {
+      res.json({ isLoggedIn: false })
+    }
   } else {
-    res.json({ success: true, message: 'No hay sesión abierta' })
+    res.json({ isLoggedIn: false })
   }
 }
 
 // metodo para usar en las demas rutas
 export function checkAuthenticated (req, res, next) {
-  if (req.isAuthenticated()) {
-    return next()
+  const token = req.headers.authorization?.split(' ')[1]
+  if (token) {
+    try {
+      jwt.verify(token, process.env.JWT_SECRET)
+      return next()
+    } catch {
+      res.status(401).json({ message: 'Unauthorized' })
+    }
+  } else {
+    res.status(401).json({ message: 'Unauthorized' })
   }
-
-  res.status(401).json({ message: 'Unauthorized' })
 }
 
 export function checkAdmin (req, res, next) {
-  if (req.isAuthenticated() && req.user.username === 'admin') {
-    return next() // Permitir acceso si es admin
+  const token = req.headers.authorization?.split(' ')[1]
+  if (token) {
+    try {
+      const user = jwt.verify(token, process.env.JWT_SECRET)
+      if (user.username === 'admin') {
+        return next()
+      }
+      return res.status(403).json({ message: 'Unauthorized' })
+    } catch {
+      return res.status(403).json({ message: 'Unauthorized' })
+    }
+  } else {
+    return res.status(403).json({ message: 'Unauthorized' })
   }
-  return res.status(403).json({ message: 'Unauthorized' })
 }
 
 export default {
   login,
-  logout
+  status
 }
