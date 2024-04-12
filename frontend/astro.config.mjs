084108830925b09f0ac66838f14ecca5514752b8
sergio zapata <sergiozap1@gmail.com>
2024-04-12 19:04:26 +0200

    config files updated

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -5,5 +5,11 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  redirects: {
+    '/old-page': {
+      status: 302,
+      destination: '/new-page'
+    }
+  }
 });
--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -1,8 +1,23 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+// OLD
+// /** @type {import('tailwindcss').Config} */
+// export default {
+// 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+// 	theme: {
+// 		extend: {},
+// 	},
+// 	plugins: [
+// 		require('tailwindcss-animated')
+// 	],
+// }
+
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
+
