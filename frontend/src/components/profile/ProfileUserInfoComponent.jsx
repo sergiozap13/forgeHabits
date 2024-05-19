29e7bc0844dcefba8e871da8d486220fc33fbfe0
sergio zapata <sergiozap1@gmail.com>
2024-05-19 20:24:43 +0200

    add tooltip and profile image working in frontend

--- frontend/src/components/profile/ProfileUserInfoComponent.jsx
+++ frontend/src/components/profile/ProfileUserInfoComponent.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import ProfileStatsComponent from './ProfileStatsComponent.jsx';
 import ProfileSettingsButtons from './ProfileSettingsButtons.jsx';
+import Tooltip from '../tooltip/Tooltip.jsx';
+import 'tippy.js/dist/tippy.css';
 
 const ProfileUserInfoComponent = () => {
     const [userInfo, setUserInfo] = useState(null);
     const token = sessionStorage.getItem('jwtToken');
-
+    const [imgUrl, setImgUrl] = useState('');
     const [isEditing, setIsEditing] = useState(false);
     const [editedUserInfo, setEditedUserInfo] = useState({ name: ''});
+    const [selectedFile, setSelectedFile] = useState(null);
 
     const handleEditClick = () => {
         if (isEditing) {
@@ -22,6 +25,10 @@ const ProfileUserInfoComponent = () => {
             ...editedUserInfo,
             [name]: value
         });
+    };
+
+    const handleFileChange = (e) => {
+        setSelectedFile(e.target.files[0]);
     };
 
     const maskEmail = (email) => {
@@ -89,10 +96,62 @@ const ProfileUserInfoComponent = () => {
         }
     }
 
+    const getProfileImage = async () => {
+        if (!userInfo?.id) return;
+        
+        try {
+            const response = await fetch(`http://localhost:3000/api/images/${userInfo.id}`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                }
+            });
+
+            if (!response.ok) throw new Error('Network response was not ok');
+
+            const blob = await response.blob();
+            const imageUrl = URL.createObjectURL(blob);
+            setImgUrl(imageUrl);
+        } catch (error) {
+            console.error("Error fetching profile image:", error);
+        }
+    };
+
+    const handleUploadClick = async () => {
+        if (!selectedFile) {
+            setError('No file selected');
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append('profileImage', selectedFile);
+
+        try {
+            const response = await fetch(`http://localhost:3000/api/upload`, {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                },
+                body: formData
+            });
+
+            if (!response.ok) throw new Error('Network response was not ok');
+
+            const imageUrl = URL.createObjectURL(selectedFile);
+            setImgUrl(imageUrl);
+        } catch (error) {
+            console.error("Error uploading profile image:", error);
+        }
+    };
+
 
     useEffect(() => {
         fetchUserInfo();
     }, []);
+
+    useEffect(() => {
+        getProfileImage();
+    }, [userInfo]);
 
     return (
         <div className="container mx-auto my-10 p-5 bg-gray-700 shadow-2xl rounded-xl">
@@ -107,10 +166,29 @@ const ProfileUserInfoComponent = () => {
             
             <div className="flex flex-col md:flex-row items-center md:items-center">
                 <div className="w-full md:w-1/3 mb-10 md:mb-0 border-2 border-gray-300 rounded-lg mx-2 bg-gray-800 flex items-center justify-center">
-                    <div className="flex flex-col items-center justify-center py-2">
-                        <img className="w-40 h-40 rounded-full border-2 border-orange-400 m-1" src="https://randomuser.me/api/portraits/men/71.jpg" alt="profile" />
-                        <h1 className="text-lg font-bold mb-3 text-center text-white">@{userInfo?.username}</h1>
-                        <button className='border-2 border-orange-400 px-3 py-1 rounded-lg'>Subir foto</button>
+                    <div className="flex flex-col items-center justify-center py-4 rounded-lg shadow-lg p-6">
+                        <Tooltip content="Que guapx estas hoy..." placement="top">
+                            <img className="w-48 h-48 rounded-full border-4 border-orange-500 mb-4 object-cover shadow-md" src={imgUrl} alt="profile" />
+                        </Tooltip>
+                        <h1 className="text-2xl font-semibold mb-2 text-center text-white">@{userInfo?.username}</h1>
+                        
+                        <Tooltip content="Sube una foto de perfil" placement="top">
+                            <input 
+                                type="file" 
+                                onChange={handleFileChange} 
+                                className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-orange-500 file:text-white hover:file:bg-orange-600"
+                            />
+                        </Tooltip>
+                        
+                        <Tooltip content="Selecciona una foto antes de pulsar" placement="bottom">
+                            <button 
+                                onClick={handleUploadClick} 
+                                className='mt-4 bg-orange-500 text-white font-semibold py-2 px-6 rounded-full hover:bg-orange-600 transition duration-300'
+                            >
+                                Subir foto
+                            </button>
+                        </Tooltip>
+                        
                     </div>
                 </div>
                 
@@ -147,10 +225,13 @@ const ProfileUserInfoComponent = () => {
                                 </p>
                             </div>
                         </div>
+                        
                         <div className="text-center py-3 my-3 w-auto">
-                            <button onClick={handleEditClick} className="border-2 border-orange-400 px-3 py-1 rounded-lg">
-                                {isEditing ? 'Confirmar' : 'Editar Nombre'}
-                            </button>
+                            <Tooltip content="Solamente puedes editar el nombre" placement="bottom">
+                                <button onClick={handleEditClick} className="border-2 border-orange-400 px-3 py-1 rounded-lg">
+                                    {isEditing ? 'Confirmar' : 'Editar Nombre'}
+                                </button>
+                            </Tooltip>
                         </div>
                     </div>
                 </div>
