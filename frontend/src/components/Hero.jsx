e60e46d94fc6df10fe9cdd59d7cd080bfe1afe18
sergio zapata <sergiozap1@gmail.com>
2024-06-02 15:48:24 +0200

    update landing page details

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,27 +2,25 @@
 function Hero() {
 
     return (    
-        <section className="pt-24 animate-fade-right animate-once animate-duration-[800ms] animate-ease-in">
-            <div className="px-12 mx-auto max-w-8xl">
+        <section className="md:pt-24 animate-fade-right animate-once animate-duration-[800ms] animate-ease-in">
+            <div className="md:px-12 mx-auto max-w-8xl">
                 <div className="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
-                    <h1 className="mb-8 text-4xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight">
+                    <h1 className="mb-8 text-4xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight text-center">
                         <span className="text-gray-200 ">¿Quieres</span> <span className="block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-orange-600 to-gray-200 lg:inline"> conseguir que tus hábitos sean duraderos?</span>
                     </h1>
-                    <p className="px-0 mb-8 text-lg text-gray-200 md:text-xl lg:px-24">
-                        Start gaining the traction you've always wanted with our next-level templates and designs. Crafted to help you tell your story.
+                    <p className="px-0 mb-8 text-lg text-gray-200 md:text-xl lg:px-24 text-center">
+                        Empieza a transformar tu vida hoy con nuestra aplicación de seguimiento de hábitos.
                     </p>
                     <div className="mb-4 space-x-0 md:space-x-2 md:mb-8">
-                        <a href="#_" className="inline-flex items-center justify-center w-full px-6 py-3 mb-2 text-lg text-white bg-orange-500 rounded-2xl sm:w-auto sm:mb-0">
+                        <a href="/register" className="inline-flex items-center justify-center w-full px-6 py-3 mb-2 text-lg text-white bg-orange-500 rounded-2xl sm:w-auto sm:mb-0">
                             Empieza ya
-                            <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                        </a>
-                        <a href="#_" className="inline-flex items-center justify-center w-full px-6 py-3 mb-2 text-lg bg-gray-100 text-gray-700 rounded-2xl sm:w-auto sm:mb-0">
-                            Más información
-                            <svg className="w-4 h-4 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"></path></svg>
+                            <span class="material-symbols-outlined">
+                                arrow_forward
+                            </span>                        
                         </a>
                     </div>
                 </div>
-                <div className="w-full mx-auto mt-20 text-center md:w-10/12">
+                <div className="w-full mx-auto md:mt-20 text-center md:w-10/12">
                     <div className="relative z-0 w-full mt-8">
                         <div className="relative overflow-hidden shadow-2xl">
                             <div className="flex items-center flex-none px-4 bg-orange-500 rounded-b-none h-11 rounded-xl">
