8de576fb65d9bb5cac7b9b3b3f75577485b18b7a
sergio zapata <sergiozap1@gmail.com>
2024-06-02 13:55:31 +0200

    update cors to vercel deploy

--- backend/src/middlewares/cors.js
+++ backend/src/middlewares/cors.js
@@ -2,8 +2,7 @@ import cors from 'cors'
 export const corsMiddleware = () => cors({
   origin: (origin, callback) => {
     const ACCEPTED_ORIGINS = [
-      'http://localhost:4321'
-    ]
+      'https://forge-habits-front.vercel.app/']
 
     if (ACCEPTED_ORIGINS.includes(origin)) {
       return callback(null, true)
