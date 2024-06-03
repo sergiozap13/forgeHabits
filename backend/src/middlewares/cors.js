7c9b472c66a7d700e3221a64b5efd5a8e2cddc82
sergio zapata <sergiozap1@gmail.com>
2024-06-03 06:52:37 +0200

    update cors for prod

--- backend/src/middlewares/cors.js
+++ backend/src/middlewares/cors.js
@@ -2,7 +2,7 @@ import cors from 'cors'
 export const corsMiddleware = () => cors({
   origin: (origin, callback) => {
     const ACCEPTED_ORIGINS = [
-      'https://forge-habits-front.vercel.app']
+      'https://forgehabits.es']
 
     if (ACCEPTED_ORIGINS.includes(origin)) {
       return callback(null, true)
